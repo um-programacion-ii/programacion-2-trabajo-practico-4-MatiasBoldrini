d20bb4b81e1cb228da1c9ab564e070489f9d6431
Matías Boldrini <50892896+MatiasBoldrini@users.noreply.github.com>
2025-05-14 09:49:14 -0300

    inicial

---
+++ gestionDeBiblioteca/src/main/java/com/biblioteca/gestionDeBiblioteca/GestionDeBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.biblioteca.gestionDeBiblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeBibliotecaApplication.class, args);
+	}
+
+}
